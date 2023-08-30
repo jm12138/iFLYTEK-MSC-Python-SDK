9803853cd5862e05a4cd1ef9302c4af0ddb9faf2
jm12138 <2286040843@qq.com>
2023-08-31 01:44:37 +0800

    fix bin files

--- setup.py
+++ setup.py
@@ -13,6 +13,9 @@ setup(
     url='https://github.com/jm12138/iFLYTEK-MSC-Python-SDK',
     packages=['msc'],
     license='Apache License 2.0',
-    data_files=[('bin', ['msc/bin/msc_x64.dll', 'msc/bin/msc_x86.dll', 'msc/bin/msc_x64.so', 'msc/bin/msc_x86.so'])],
+    include_package_data=True,
+    package_data={
+        'bin': ['*.dll', '*.so']
+    },
     platforms=['win32', 'win-amd64', 'linux-i686', 'linux-x86_64']
 )
