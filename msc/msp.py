17e33ac2d48187dd29cb9690a092ce16c8d683b0
jm12138 <2286040843@qq.com>
2024-01-27 15:42:30 +0800

    fix load dll

--- msc/msp.py
+++ msc/msp.py
@@ -1,5 +1,4 @@
 import os
-import platform
 
 from ctypes import cdll
 from ctypes import CDLL
@@ -9,35 +8,10 @@ from ctypes import byref, string_at
 from ctypes import c_int, c_uint, c_void_p, c_char_p
 
 
-def LoadLibrary(
-    file_name: str, base_dir: str = os.path.dirname(os.path.abspath(__file__))
-) -> CDLL:
-    try:
-        file_path = os.path.join(base_dir, "bin", file_name)
-        msc = cdll.LoadLibrary(file_path)
-    except Exception as e:
-        raise Exception("Failed to load library: %s" % e)
-    return msc
-
-
 def LoadMSC() -> CDLL:
-    system = platform.system()
-    arch = platform.architecture()[0]
-
-    if system == "Windows":
-        if arch == "64bit":
-            msc = LoadLibrary("msc_x64.dll")
-        else:
-            msc = LoadLibrary("msc_x86.dll")
-    elif system == "Linux":
-        if arch == "64bit":
-            msc = LoadLibrary("msc_x64.so")
-        else:
-            msc = LoadLibrary("msc_x86.so")
-    else:
-        raise Exception("Unsupported system: %s" % system)
-    return msc
-
+    name = os.getenv('MSC_SDK_PATH')
+    assert name is not None, 'MSC_SDK_PATH is not set.'
+    return cdll.LoadLibrary(name)
 
 msc = LoadMSC()
 
